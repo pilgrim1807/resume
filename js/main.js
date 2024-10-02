435da8b7d57a52b20a16bae502c41d0ef9fd7966
pilgrim1807 <51112693+pilgrim1807@users.noreply.github.com>
2024-10-02 16:45:55 +0300

    Проект готов

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//var num;
+//num = 5;
+//const num = 5;
+//num = 7;
+//console.log("Переменная: " + num + ".");
+// const countChars = (str, char) => {
+//     let i = 0;
+//     let count = 0;
+//     while (i < str.length) {
+//       if (str[i] === char) {
+
+//         count = count + 1;
+//       }
+
+//       i = i + 1;
+//     }
+  
+//     return count;
+//   };
+
+
